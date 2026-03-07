22e738eba273f3695479b4544976706c8eed8183
vishal-waghmare <vishalwaghmare0045@yahoo.com>
2026-03-07 14:56:07 +0530

    Added send notification

--- local_notification/lib/features/local_nofitication/send_notification.dart
+++ local_notification/lib/features/local_nofitication/send_notification.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
 class SendNotificationScreen extends StatefulWidget {
@@ -8,6 +9,60 @@ class SendNotificationScreen extends StatefulWidget {
 }
 
 class _SendNotificationScreenState extends State<SendNotificationScreen> {
+
+  Future<void> _initializeNotification() async{
+    AwesomeNotifications().initialize(
+        null,
+        [
+          NotificationChannel(
+              channelGroupKey: 'basic_channel_group',
+              channelKey: 'basic_channel',
+              channelName: 'Basic notification',
+              channelDescription: 'Notification channel for test',
+              defaultColor: Colors.deepPurple,
+              ledColor: Colors.white
+          )
+        ],
+      channelGroups: [
+        NotificationChannelGroup(
+            channelGroupKey: 'basic_channel_group',
+            channelGroupName: 'Basic group'
+        )
+      ],
+      debug: true,
+    );
+  }
+
+  Future<void> sendNotification() async{
+    bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
+    if(!isAllowed) isAllowed = await displayNotificationRational();
+    if(!isAllowed) return;
+
+    AwesomeNotifications().createNotification(
+        content: NotificationContent(
+            id: 10,
+            channelKey: 'basic_channel',
+            actionType: ActionType.Default,
+            title: 'Hello Notification',
+            body: 'This is my first notification',
+        )
+    );
+  }
+
+  static Future<bool> displayNotificationRational()async{
+    bool isAllowed = await AwesomeNotifications().requestPermissionToSendNotifications();
+
+    return isAllowed;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(microseconds: 3), ()async{
+      await _initializeNotification();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +70,9 @@ class _SendNotificationScreenState extends State<SendNotificationScreen> {
         child: Padding(
           padding: const EdgeInsets.all(20),
           child: InkWell(
-            onTap: (){},
+            onTap: (){
+              sendNotification();
+            },
             child: Container(
               width: double.infinity,
               height: 60,
